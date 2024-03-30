cc33e8d152a3cd8fa0b8d26dd19d7eb3ed7c95c9
Alperen İlgaz <alperenilgaz3@gmail.com>
2024-03-30 13:52:14 +0300

    stripe frontend process completed

--- frontend/src/components/Cart/CartTotals.jsx
+++ frontend/src/components/Cart/CartTotals.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { CartContext } from '../../context/CartContext'
-
+import {message} from 'antd'
+import {loadStripe} from '@stripe/stripe-js'
 const CartTotals = () => {
     const {cardItem} = useContext(CartContext)
     const [isCheked, setIsCheked] = useState(false)
+    const user = localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")):null
     const CardItemsTotal = cardItem.map((item) => {
         const itemTotal = item.price * item.quantity
         return itemTotal
@@ -14,7 +16,42 @@ const CartTotals = () => {
       },0)
       
       const cargoPrice = 15
+
       const cartTotals = isCheked ? subTotal+cargoPrice : subTotal
+
+      const stripePublicKey = VITE_API_STRIPE_PUBLIC_KEY
+      const apiUrl = import.meta.env.VITE_API_BASE_URL
+      const handlePayment = async() => {
+        if(!user){
+            return message.info("Ödeme yapmak için giriş yapmalısınız")
+        }
+        const body = {
+            products:cardItem,
+            user: user,
+            cargoPrice: isCheked ? cargoPrice :0
+        }
+        try {
+            const stripe = await loadStripe(stripePublicKey)
+            const res = await fetch(`${apiUrl}/api/payment`,{
+                method:"POST",
+                headers:{"Content-Type":"application/json"},
+                body: JSON.stringify(body)
+            })
+            if(!res.ok){
+                return message.error("Ödeme işlemi başarısız oldu!")
+            }
+            const session = await res.json()
+            const result = await stripe.redirectToCheckout({
+                sessionId:session.id
+            })
+            if(result.error){
+            throw new Error(result.error.message)
+            }
+        } catch (error) {
+
+            
+        }
+      }
   return (
     <div className="cart-totals">
                     <h2>Cart totals
@@ -52,7 +89,7 @@ const CartTotals = () => {
                         </tbody>
                     </table>
                     <div className="checkout">
-                        <button className="btn btn-lg">Proceed to checkout</button>
+                        <button onClick={handlePayment} className="btn btn-lg">Proceed to checkout</button>
                     </div>
                 </div>
   )
